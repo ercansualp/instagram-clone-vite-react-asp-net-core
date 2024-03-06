108dce2f1a2fc726250aa0b07c7f860500e62725
Ercan SUALP <ercansualp@outlook.com.tr>
2024-03-06 15:40:11 +0300

    The footer section is still under development, and a loading screen has been added.

--- instagram-clone-client/src/layouts/main/footer/index.tsx
+++ instagram-clone-client/src/layouts/main/footer/index.tsx
@@ -1,7 +1,10 @@
-import {ExploreIcon, HomeIcon, MessengerIcon, NewPostIcon, ReelsIcon} from "~/assets/icons.tsx";
+import {HomeIcon, MessengerIcon, ReelsIcon, SearchIcon} from "~/assets/icons.tsx";
 import FooterItem from "~/layouts/main/footer/footer-item";
 import HeadlessUIDialog from "~/components/headlessUIDialog";
 import NewPost from "~/components/new-post";
+import {NavLink} from "react-router-dom";
+import UserAvatar from "~/assets/img/user.jpg";
+import classNames from "classnames";
 
 const footerItems = [
     {
@@ -11,30 +14,24 @@ const footerItems = [
     },
     {
         url: "explore",
-        activeIcon: <ExploreIcon width={24} height={24} active={true} />,
-        passiveIcon: <ExploreIcon width={24} height={24} active={false} />
+        activeIcon: <SearchIcon width={24} height={24} active={true} />,
+        passiveIcon: <SearchIcon width={24} height={24} active={false} />
     },
     {
         url: "reels",
         activeIcon: <ReelsIcon width={24} height={24} active={true} />,
         passiveIcon: <ReelsIcon width={24} height={24} active={false} />
     },
-    <NewPostIcon width={24} height={24} />,
     {
         url: "direct/inbox",
         activeIcon: <MessengerIcon width={24} height={24} active={true} />,
         passiveIcon: <MessengerIcon width={24} height={24} active={false} />
-    },
-    {
-        url: "/",
-        activeIcon: <HomeIcon width={24} height={24} active={true} />,
-        passiveIcon: <HomeIcon width={24} height={24} active={false} />
     }
 ];
 
 export default function Footer() {
     return (
-        <div className="h-[50px]">
+        <div className="h-[50px] fixed bottom-0 left-0 w-full">
             <div className="h-[49px] border-t border-t-[#363636] flex flex-row justify-evenly">
                 <div className="flex justify-evenly w-full">
                     {
@@ -46,6 +43,13 @@ export default function Footer() {
                             )
                         ))
                     }
+                    <NavLink to={"ercan"} className="w-12 h-12 p-3">
+                        {({ isActive }) => (
+                            <img src={UserAvatar} alt="" width={25.2} height={25.2} className={classNames("rounded-full", {
+                                "border-2 border-[#f5f5f5]": isActive
+                            })}/>
+                        )}
+                    </NavLink>
                 </div>
             </div>
         </div>
--- instagram-clone-client/src/routes/Loading.tsx
+++ instagram-clone-client/src/routes/Loading.tsx
@@ -1,7 +1,13 @@
+import LoadingIcon from "~/assets/img/loading.png";
+import FromMeta from "~/assets/img/from_meta.png";
+
 export default function Loading() {
     return (
-        <>
-            Loading Page
-        </>
+        <div className="h-screen flex items-center justify-center">
+            <img src={LoadingIcon} alt="" width={80} height={80} />
+            <span className="w-[72px] h-[41px] -mt-9 -ml-9 absolute bottom-8 inset-x-1/2">
+                <img src={FromMeta} alt="" width={72} height={37} />
+            </span>
+        </div>
     )
 }
